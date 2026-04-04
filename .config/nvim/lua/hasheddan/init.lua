6ee2d0ffa45d01b2f2e635c83638290b2de8d2e2
hasheddan <georgedanielmangum@gmail.com>
2026-04-04 09:41:03 -0400

    nvim: lsp: organize Go imports with gopls
    Adds a BufWritePre autocommand for organizing imports in Go files.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- .config/nvim/lua/hasheddan/init.lua
+++ .config/nvim/lua/hasheddan/init.lua
@@ -42,5 +42,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "]d", function()
 			vim.diagnostic.goto_prev()
 		end, opts)
+		-- The following section is directly from the gopls documentation: https://go.dev/gopls/editor/vim#neovim-imports
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				local params = vim.lsp.util.make_range_params()
+				params.context = { only = { "source.organizeImports" } }
+				local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+				for cid, res in pairs(result or {}) do
+					for _, r in pairs(res.result or {}) do
+						if r.edit then
+							local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+							vim.lsp.util.apply_workspace_edit(r.edit, enc)
+						end
+					end
+				end
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 	end,
 })
