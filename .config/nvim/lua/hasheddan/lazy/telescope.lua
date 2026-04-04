ec2c8d2556ae40dc13baeb1698e998eb56071c4c
hasheddan <georgedanielmangum@gmail.com>
2026-04-04 10:01:24 -0400

    nvim: telescope: setup key mappings
    Adds basic key mappings for telescope.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- .config/nvim/lua/hasheddan/lazy/telescope.lua
+++ .config/nvim/lua/hasheddan/lazy/telescope.lua
@@ -5,4 +5,38 @@ return {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			mode = "n",
+			desc = "Telescope Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			mode = "n",
+			desc = "Telescope Live Grep",
+		},
+		{
+			"<leader>fw",
+			function()
+				require("telescope.builtin").grep_string()
+			end,
+			mode = "n",
+			desc = "Telescope Grep String",
+		},
+		{
+			"<leader>ss",
+			function()
+				require("telescope.builtin").spell_suggest()
+			end,
+			mode = "n",
+			desc = "Telescope Spell Suggest",
+		},
+	},
 }
